9b92a151a467c8096167b7bc2ad6f71e76b28811
Elif Rana <elifranaaltintas2@gmail.com>
2025-05-14 23:06:11 +0300

    Pyhton 4 işlem

--- 01_Lab.py
+++ 01_Lab.py
@@ -24,8 +24,54 @@
 #* Boşluk barındırmamalı firstName
 #* Değişken isimlerini yaptığı işle ilintili olmasını telkin
 
-first_name = 10
+# first_name = 10
 
-print(first_name)
-print(type(first_name)) #? burada tipi integer yani tam sayı
+# print(first_name)
+# print(type(first_name)) #? burada tipi integer yani tam sayı
+
+
+#! 2 tane sayıyı toplayıp ekrana yazdıran uyguglamayı yazınız
+# sayi_1 = 5
+# sayi_2 = 10
+
+# toplam = sayi_1 + sayi_2
+
+# print(toplam)
+
+# #! yukseklik ve taban bilgisi bilinen bir üçgenin alanını hesaplayın
+# yukseklik = 10
+# taban = 20
+
+# alan = yukseklik * taban / 2
+
+# print(alan)
+
+# #! Kullanıcıdan alınan 2 adet sayıyı çarpan uygulamayı yazınız
+# number_1 = int(input('Lütfen sayı giriniz: '))
+# number_2 = int(input('Lütfen sayı giriniz: '))
+# print(type(number_1))
+# carpim = number_1 * number_2
+
+# print(carpim)
+
+# #! Kulanıcıdan alınan kenar bilgisine göre karenin alanını ve çevresini hesaplayan kodu yazınız lütfen
+# edge = int(input('Edge: '))
+
+# area = edge * edge
+# premiter = 4 * edge
+
+# print(f'Area: {area}')
+# print(f'Premiter: {premiter}')
+
+# print('Area', area)
+# print('Area: {}'.format(area))
+
+# #! Dikdörtgenin çevresini ve alanını hesaplayın
+# kisa_kenar = int(input('Kısa Kenar: '))
+# uzun_kenar = int(input('Uzun kenar: '))
+
+# alan = kisa_kenar * uzun_kenar
+# cevre = (kisa_kenar + uzun_kenar) * 2
+
+# print(f'Alan: {alan}\nÇevre: {cevre}')
 
